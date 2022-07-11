ce32f97c5e605200612c0fc02614c2dcaab5d672
juan silva <juan_silva_o@hotmail.com>
2022-07-10 20:28:40 -0400

    Errores tabla responsive

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["fullName"] = document.getElementById("fullName").value;
-    formData["email"] = document.getElementById("email").value;
     formData["phone"] = document.getElementById("phone").value;
     formData["description"] = document.getElementById("description").value;
     return formData;
@@ -25,13 +24,11 @@ function insertNewRecord(data) {
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
-    cell3 = newRow.insertCell(2);
+    cell3 = newRow.insertCell(1);
     cell3.innerHTML = data.phone;
-    cell4 = newRow.insertCell(3);
+    cell4 = newRow.insertCell(2);
     cell4.innerHTML = data.description;
-    cell4 = newRow.insertCell(4);
+    cell4 = newRow.insertCell(3);
     cell4.innerHTML = `
     <a type="button" class="btn btn-warning" onClick="onEdit(this)">Editar</a>
     <a type="button" class="btn btn-danger" onClick="onDelete(this)">Borrar</a>`;
@@ -39,7 +36,6 @@ function insertNewRecord(data) {
 
 function resetForm() {
     document.getElementById("fullName").value = "";
-    document.getElementById("email").value = "";
     document.getElementById("phone").value = "";
     document.getElementById("description").value = "";
     selectedRow = null;
@@ -48,15 +44,14 @@ function resetForm() {
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("phone").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("description").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("phone").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("description").value = selectedRow.cells[2].innerHTML;
+
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.email;
-    selectedRow.cells[2].innerHTML = formData.phone;
-    selectedRow.cells[3].innerHTML = formData.description;
+    selectedRow.cells[1].innerHTML = formData.phone;
+    selectedRow.cells[2].innerHTML = formData.description;
 }
 
 function onDelete(td) {
